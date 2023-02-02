01afabbbb9d10931de1a95318b0b25eda5735bca
guna17215 <109857836+guna17215@users.noreply.github.com>
2023-02-02 16:34:18 +0530

    Update opps.cpp

--- opps.cpp
+++ opps.cpp
@@ -123,3 +123,126 @@ int main(){
     }
     return 0;
 }
+
+
+
+// program 3
+
+#include <iostream>
+using namespace std;
+
+class Employee{
+    long long employeeNo;
+    string employeeName;
+    float basicSalary;
+    float DA;
+    float IT;
+    float netSalary;
+    float grossSalary;
+    public:
+    void readData(){
+        cout << "ENTER THE EMPLOYEE ID, NAME AND BASICSALARY\n";
+        cin >> employeeNo >> employeeName >> basicSalary;
+    }
+    void calcNetsalary(){
+        DA = (0.52)*basicSalary;
+        grossSalary = DA + basicSalary;
+        IT = (0.3)*grossSalary;
+        netSalary = DA + basicSalary - IT;
+    }
+    void printData(){
+        cout << "EmployeeName:" << employeeName << endl;
+        cout << "EmpoyeeId:" << employeeNo << endl;
+        cout << "BasicSalary:" << basicSalary << endl;
+        cout << "DA:" << DA << endl;
+        cout << "IT" << IT << endl;
+        cout << "GrossSalary:" << grossSalary << endl;
+        cout << "NetSalary:" << netSalary << endl;
+    }
+};
+
+int main(){
+    int n;
+    cout << "Enter no of employee's\n";
+    cin >> n;
+    Employee e[n], *p;
+    for(int i = 0; i < n; i++){
+        p = &e[i];
+        p->readData();
+        p->calcNetsalary();
+    }
+    for(int i = 0; i < n; i++){
+        printf("<<<------------------------------>>>\n");
+        p = &e[i];
+        p->printData();
+        printf("<<<------------------------------>>>\n");
+    }
+    return 0;
+}
+
+
+
+// program 4
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Student{
+    // public:
+    string USN;
+    string Name;
+    float avg;
+    float marks[3];
+    public:
+    Student(){
+        cout << "ENTER THE USN, NAME AND MARKS\n";
+        cin >> USN >> Name >> marks[0] >> marks[1] >> marks[2];
+    }
+    
+    void calcAvgofbestoftwo(){
+        sort(marks, marks+3);
+        avg = (marks[1]+marks[2])/2;
+    }
+    void printData(){
+        cout << "StudentName:" << Name << endl;
+        cout << "USN:" << USN << endl;
+        cout << "Average of best of 2 marks:" << avg << endl;
+    }
+    friend void topper(int n, Student s[]);
+};
+
+void topper(int n, Student s[]){
+        float totalmarks, maxmarks = 0;
+        string tomper;
+        for(int i = 0; i < n; i++){
+            totalmarks = s[i].marks[0]+s[i].marks[1]+s[i].marks[2];
+            if(maxmarks < totalmarks){
+                tomper = s[i].Name;
+                maxmarks = totalmarks;
+            }
+        }
+        cout << tomper << "is the topper" << endl;
+}
+
+int main(){
+    int n;
+    cout << "Enter no of employee's\n";
+    cin >> n;
+    Student s[n];
+    for(int i = 0; i < n; i++){
+        s[i].calcAvgofbestoftwo();
+    }
+    for(int i = 0; i < n; i++){
+        printf("<<<------------------------------>>>\n");
+        s[i].printData();
+        printf("<<<------------------------------>>>\n");
+    }
+    topper(n, s);
+    return 0;
+}
+
+
+// program 5
+
+
