ea36a869d8f135f6715bf845ba2910c694491be6
guna17215 <109857836+guna17215@users.noreply.github.com>
2023-02-02 12:14:09 +0530

    Update opps.cpp

--- opps.cpp
+++ opps.cpp
@@ -1,3 +1,66 @@
+// program 1:
+
+#include <iostream>
+using namespace std;
+void copyString(){
+    int i = 0;
+    char str1[100];
+    char str2[100];
+    cout << "ENTER THE STRING-1\n";
+    cin >> str1;
+    while(str1[i] != '\0'){
+        str2[i] = str1[i];
+        i++;
+    }
+    str2[i] = '\0';
+    cout << "ENTERED STRING IS: " << str1 << endl;
+    cout << "COPYED STRING IS: " <<  str2 << endl;
+}
+
+int strLen(char str[]){
+    int len = 0;
+    while(str[len] != '\0'){
+        len++;
+    }
+    return len;
+}
+
+void concatString(){
+    char str1[100];
+    char str2[100];
+    cout << "ENTER THE STRING-1\n";
+    cin >> str1;
+    cout << "ENTER THE STRING-2\n";
+    cin >> str2;
+    int len1 = strLen(str1);
+    int len2 = strLen(str2);
+    int i;
+    for(i = len1; i < len1+len2; i++)
+    str1[i] = str2[i-len1];
+    str1[i] = '\0';
+    cout << "CONCAATENATED STRING IS: " << str1 << endl;
+}
+
+int main(){
+    int ch, stop = 1;
+    while(stop){
+        cout << "1.COPY STRING\n2.CONCATENATE STRING\n";
+        cin >> ch;
+        switch(ch){
+          case 1: copyString();
+          break;
+          case 2: concatString();
+          break;
+          default: cout << "INVALID CHOICE\n";
+        }
+        cout << "DO YOU WANA PERFORM ANOTHER OPERATION if yes press 0 else 1\n";
+        cin >> stop;
+    }
+    return 0;
+}
+
+
+
 // program 2:
 
 #include <iostream>
